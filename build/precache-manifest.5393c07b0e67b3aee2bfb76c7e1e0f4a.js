0314f5ecb5d9861cafc7aafb87173e8db3802e4b
Neliubin Oleksiy <aa7n2012@gmail.com>
2020-10-01 13:16:56 +0300

    A-Q-H_v4

---
+++ build/precache-manifest.5393c07b0e67b3aee2bfb76c7e1e0f4a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526a662ed000549d1eec5e0efef90af7",
+    "url": "/Air-Quality-Health/index.html"
+  },
+  {
+    "revision": "b447a0ee9dfd4a3b02d8",
+    "url": "/Air-Quality-Health/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "a549b82ade2f68684497",
+    "url": "/Air-Quality-Health/static/css/main.ff0c1a0d.chunk.css"
+  },
+  {
+    "revision": "b447a0ee9dfd4a3b02d8",
+    "url": "/Air-Quality-Health/static/js/2.26cdb5ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Air-Quality-Health/static/js/2.26cdb5ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a549b82ade2f68684497",
+    "url": "/Air-Quality-Health/static/js/main.4bbecf53.chunk.js"
+  },
+  {
+    "revision": "5e91c32034325c1b0b2b",
+    "url": "/Air-Quality-Health/static/js/runtime-main.e75318d5.js"
+  },
+  {
+    "revision": "e60be19e8cbfbd550ac9a3f905ad359a",
+    "url": "/Air-Quality-Health/static/media/GC_logo.e60be19e.svg"
+  },
+  {
+    "revision": "899cd2784349ab5b5a24f5dcee2ffca3",
+    "url": "/Air-Quality-Health/static/media/ITN_logo.899cd278.svg"
+  },
+  {
+    "revision": "f8f67844654cd9fc78dd57834ecfed6c",
+    "url": "/Air-Quality-Health/static/media/city_planing.f8f67844.jpg"
+  },
+  {
+    "revision": "43f9dac62ffb9209e823b91b7504c5ec",
+    "url": "/Air-Quality-Health/static/media/energetics.43f9dac6.jpg"
+  },
+  {
+    "revision": "fb084e1e416e340515649cb48eb04f6e",
+    "url": "/Air-Quality-Health/static/media/energy.fb084e1e.jpg"
+  },
+  {
+    "revision": "5d86912d3747f4d2fb2419bd7d2de74b",
+    "url": "/Air-Quality-Health/static/media/hero-bg.5d86912d.jpg"
+  },
+  {
+    "revision": "5ec44869469e2f3c9db3551c44662f88",
+    "url": "/Air-Quality-Health/static/media/industry.5ec44869.jpg"
+  },
+  {
+    "revision": "8dd4ef7149a55ade70696aa0e9e2faa3",
+    "url": "/Air-Quality-Health/static/media/shid_emozhl_logo.8dd4ef71.svg"
+  },
+  {
+    "revision": "7e1db0da3d25263be6534e000b29d2a8",
+    "url": "/Air-Quality-Health/static/media/slide-1.7e1db0da.jpg"
+  },
+  {
+    "revision": "12a25e248f105a327021b69336c5b71b",
+    "url": "/Air-Quality-Health/static/media/slide-2.12a25e24.jpg"
+  },
+  {
+    "revision": "638ec2275a294137e028e6bce58ad768",
+    "url": "/Air-Quality-Health/static/media/slide-3.638ec227.jpg"
+  },
+  {
+    "revision": "8a21e8b7fc0908ab482cc0bb0c98138a",
+    "url": "/Air-Quality-Health/static/media/slide-4.8a21e8b7.jpg"
+  },
+  {
+    "revision": "bb9f49bba9c380e18e77de4c1ed82bf6",
+    "url": "/Air-Quality-Health/static/media/slide-5.bb9f49bb.jpg"
+  },
+  {
+    "revision": "65643b05446e10934c8778999860e9de",
+    "url": "/Air-Quality-Health/static/media/transport.65643b05.jpg"
+  },
+  {
+    "revision": "a8b6caeec11d336276f5ed05a63fc938",
+    "url": "/Air-Quality-Health/static/media/utilisation.a8b6caee.jpg"
+  }
+]);
